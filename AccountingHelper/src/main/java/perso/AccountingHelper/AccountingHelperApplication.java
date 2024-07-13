475294a932a9b52671023accdc6c674d7c5a389f
Sleepy Grizzly <nicolas.bessonnet96@orange.com>
2024-07-13 18:02:47 +0200

    Spring initialzr

---
+++ AccountingHelper/src/main/java/perso/AccountingHelper/AccountingHelperApplication.java
@@ -0,0 +1,13 @@
+package perso.AccountingHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingHelperApplication.class, args);
+	}
+
+}
---
+++ AccountingHelper/src/test/java/perso/AccountingHelper/AccountingHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package perso.AccountingHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
